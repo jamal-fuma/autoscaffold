351ba27bb15e706e35b38e9ccb092151439dac4e
Jamal Natour <Jamal.Natour@fumasoftware.com>
2017-04-24 12:17:47 +0100

    Patch: unleash lots of extra warnings

--- am/test_common.mk
+++ am/test_common.mk
@@ -21,16 +21,22 @@ LINKER_FLAGS	= $(BOOST_LDFLAGS) \
 				  $(COVERAGE_LDFLAGS) \
 				  -rdynamic
 
-WARNING_FLAGS	= -Wall -Wextra -pedantic \
-		  -Wunused-value \
-		  -Wcast-align \
-		  -Wno-unused-parameter \
-		  -Wunused-variable \
+WARNING_FLAGS	= -Wall -Wextra \
+		   -fno-omit-frame-pointer \
+		   -pedantic -pedantic-errors \
+		  -Wunused-value -Wunused -Wunused-parameter -Wunused-variable \
+		  -Wunreachable-code \
+		  -Wcast-align -Wcast-qual \
 		  -Winit-self \
 		  -Wfloat-equal \
-		  -Wno-undef \
-		  -Wno-shadow \
-		  -Wcast-qual \
+		  -Wundef \
+		  -Wredundant-decls \
+		  -Wshadow -Wstack-protector \
+		  -Wdisabled-optimization \
+		  -Wmissing-field-initializers -Wmissing-format-attribute -Wformat-nonliteral -Wformat-security -Wformat-y2k \
+		  -Wmissing-noreturn \
+		  -Wimport  -Winit-self  -Winline \
+		  -Wstrict-aliasing=2 \
 		  -Wwrite-strings
 
 if FREEBSD
